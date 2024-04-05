bfa92510d478bded6caf3f11ad391f65742a6456
Leonid Lezner <kontakt@lezner.de>
2024-04-05 17:46:51 +0200

    Building

--- admin/excalidraw-block/build/index.asset.php
+++ admin/excalidraw-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'bf5e0d8786c1b3bd39a5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '4d58efa9e1880ed8afce');
